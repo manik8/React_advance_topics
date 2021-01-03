0e14ea46f50b4a6c28d9ee47f44594473a7c933f
Manik Sharma <maniksharma2014.ms@gmail.com>
2021-01-04 00:29:42 +0530

    useState-object started

---
+++ src/tutorial/1-useState/4-useState-object.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message"
+  });
+
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+  const changeMessage = () => {
+    setMessage("Hello World");
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+    </>
+  );
+};
+
+export default UseStateObject;
