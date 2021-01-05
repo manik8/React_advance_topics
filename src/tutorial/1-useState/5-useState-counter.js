fd3eb7c201db555c37d53f7fb8c712b13c2aabb0
Manik Sharma <maniksharma2014.ms@gmail.com>
2021-01-06 02:24:18 +0530

    useState-counter-project-completed

--- src/tutorial/1-useState/5-useState-counter.js
+++ src/tutorial/1-useState/5-useState-counter.js
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
   return (
     <>
-      <section style={{margin: '4rem 0'}}>
+      <section style={{ margin: "4rem 0" }}>
         <h2>Regular Counter</h2>
         <h1>{value}</h1>
-        <button className="btn" onClick={() => setValue(value-1)}>decrease</button>
-        <button className="btn" onClick={() => setValue(0)}>reset</button>
-        <button className="btn" onClick={() => setValue(value+1)}>increase</button>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          increase
+        </button>
       </section>
 
-       <section style={{margin: '4rem 0'}}>
+      <section style={{ margin: "4rem 0" }}>
         <h2>More Complex Counter</h2>
         <h1>{value}</h1>
-        <button className="btn" onClick={complexIncrease}>Increase Later</button>
+        <button className="btn" onClick={complexIncrease}>
+          Increase Later
+        </button>
       </section>
-
     </>
   );
 };
