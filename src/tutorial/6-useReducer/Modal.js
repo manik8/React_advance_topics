23893b302475ae90257b27c4a3b9301398909abc
Manik Sharma <maniksharma2014.ms@gmail.com>
2021-01-18 12:58:52 +0530

    useReducer-modal

---
+++ src/tutorial/6-useReducer/Modal.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+
+const Modal = ({ modalContent, closeModal }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      closeModal();
+    }, 2000);
+    return () => clearTimeout(timeout);
+  });
+
+  return (
+    <div className="modal">
+      <p>{modalContent}</p>
+    </div>
+  );
+};
+
+export default Modal;
