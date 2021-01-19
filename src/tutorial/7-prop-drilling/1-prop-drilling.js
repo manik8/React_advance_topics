f7e10e91846ddcad32bd26e31368b8868e19837e
Manik Sharma <maniksharma2014.ms@gmail.com>
2021-01-20 01:12:30 +0530

    prop-drilling-continuation

---
+++ src/tutorial/7-prop-drilling/1-prop-drilling.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { data } from "../../../data";
+
+// more components
+// fix - context api, redux (for more complex cases)
+
+const PropDrilling = () => {
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+
+  return (
+    <section>
+      <h3>Prop Drilling</h3>
+      <List people={people} removePerson={removePerson} />
+    </section>
+  );
+};
