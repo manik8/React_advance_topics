8c7650b8861b36fa64ab9e188ccf4f6daa27c20c
Manik Sharma <maniksharma2014.ms@gmail.com>
2021-01-26 23:15:37 +0530

    useEffect-cleanUp

---
+++ src/tutorial/2-useEffect/2-useEffect-cleanup.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+// cleanup function
+// second argument
+
+const UseEffectCleanup = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+  });
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
+};
+
+export default UseEffectCleanup;
